de25c90ff2d9c9f910d9add07c3f0e7084b8cb46
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-19 23:53:40 -0400

    Class level RequestMapping

--- src/com/mc/controller/HelloController.java
+++ src/com/mc/controller/HelloController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/greet")
 public class HelloController {
 
 	@RequestMapping("/welcome")
