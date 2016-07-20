8e64ed54dab84ad37b6f84e3dcd3d064e6b1330f
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-19 23:45:14 -0400

    Using Component Annotation to create bean

--- src/com/mc/controller/HelloController.java
+++ src/com/mc/controller/HelloController.java
@@ -1,20 +1,32 @@
 package com.mc.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
- 
-public class HelloController extends AbstractController{
- 
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request,
-		HttpServletResponse response) throws Exception {
- 
-		ModelAndView modelandview = new ModelAndView("index");
-		modelandview.addObject("welcomeMessage", "Hi User, welcome to the first Spring MVC Application");
-		
-		return modelandview;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView helloWorld() {
+
+		ModelAndView model = new ModelAndView("index");
+		model.addObject("welcomeMessage","hello world");
+
+		return model;
 	}
+
 }
+
+//public class HelloController extends AbstractController{
+// 
+//	@Override
+//	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+//		HttpServletResponse response) throws Exception {
+// 
+//		ModelAndView modelandview = new ModelAndView("index");
+//		modelandview.addObject("welcomeMessage", "Hi User, welcome to the first Spring MVC Application");
+//		
+//		return modelandview;
+//	}
+//}
