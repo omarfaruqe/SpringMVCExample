d7347e75a9f8750b9de4930a56046edd7c0911ae
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-20 00:16:51 -0400

    @PathVariable annotation with Map

--- src/com/mc/controller/HelloController.java
+++ src/com/mc/controller/HelloController.java
@@ -1,17 +1,26 @@
 package com.mc.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 //@RequestMapping("/greet")
 public class HelloController {
 
 	@RequestMapping("/welcome/{countryName}/{userName}")
-	public ModelAndView helloWorld(@PathVariable("userName") String name, @PathVariable("countryName") String country) {
+//	public ModelAndView helloWorld(@PathVariable("userName") String name, @PathVariable("countryName") String country) {
+	
+	public ModelAndView helloWorld(@PathVariable Map<String, String> pathVars) {
 
+		String name = pathVars.get("userName");
+		String country = pathVars.get("countryName");
+		
 		ModelAndView model = new ModelAndView("index");
 		model.addObject("welcomeMessage","hello "+name+" You are from "+country);
 
