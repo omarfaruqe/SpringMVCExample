ea288529622eaaf85eb464b568a0ff98e6b76d92
Omar Faruqe <faruqe.cse@gmail.com>
2016-07-20 22:31:08 -0400

    page browsing observer interceptor implemented

--- src/com/mc/admissioncontroller/DayOfWeekBasedAccessInterceptor.java
+++ src/com/mc/admissioncontroller/DayOfWeekBasedAccessInterceptor.java
@@ -2,9 +2,11 @@ package com.mc.admissioncontroller;
 
 import java.util.Calendar;
 
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.servlet.handler.*;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter{
 
@@ -24,7 +26,17 @@ public class DayOfWeekBasedAccessInterceptor extends HandlerInterceptorAdapter{
 			}
 			
 			return true;
-
 	}
-
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		// this method would be called after Spring MVC executes the request handler method for the request
+		System.out.println("HandlerInterceptorAdapter : Spring MVC called postHandle method for " + request.getRequestURI().toString() );
+	}
+	
+	@Override
+	public void afterCompletion(HttpServletRequest request,	HttpServletResponse response, Object handler, Exception ex)	throws Exception {	
+		// this method would be called after the response object is produced by the view for the request
+		System.out.println("HandlerInterceptorAdapter : Spring MVC called afterCompletion method for " + request.getRequestURI().toString());
+	}
 }
